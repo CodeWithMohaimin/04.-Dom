b970bd9b56a1d0decb9690fc9dbe6c3edc31476b
programmermohaimin <you@example.com>
2022-05-29 17:01:12 +0600

    starting tost message

--- app.js
+++ app.js
@@ -3,37 +3,29 @@ window.onload = () => {
     main()
 }
 function main() {
-    // collect all references
     const root = document.getElementById('root');
-    const btn = document.getElementById('btn');
+    const copy_btn = document.getElementById('copy_btn');
+    const change_btn = document.getElementById('change_btn');
     const output = document.getElementById('output');
-    const copyBtn = document.getElementById('copy_btn');
-
-    btn.addEventListener('click', function () {
-
+    
+    change_btn.addEventListener('click', function () {
         const bgColor = hex();
         root.style.backgroundColor = bgColor;
+
         output.value = bgColor;
-
-    }) 
-
-    copyBtn.addEventListener('click', function () {
-        
+    })
+    copy_btn.addEventListener('click', function () {
         navigator.clipboard.writeText(output.value)
     })
-
-    
-        
 }
 
+//Generate Hex color
 
-//generate Color code function
 function hex() {
-    
-    const red = Math.floor(Math.random() * 255);
-    const green = Math.floor(Math.random() * 255);
+    const red = Math.floor(Math.random()*255);
+    const green = Math.floor(Math.random()*255);
     const blue = Math.floor(Math.random() * 255);
 
-    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
-
+    // return `rgb(${red},${green},${blue})`
+    return `#${red.toString(16)}${blue.toString(16)}${green.toString(16)}`
 }
